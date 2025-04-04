aa3ee6e5ecb0127f497fe85cb1196af5f57b8c5e
zanelamb2 <zane.lamb@datacom.co.nz>
2025-04-04 14:46:52 +1300

    add base test setup

--- DatapayCalculatorTests/TaxCalculatorTests.cs
+++ DatapayCalculatorTests/TaxCalculatorTests.cs
@@ -1,9 +1,23 @@
+using Xunit;
 using DatapayCalculatorTest;
+using NUnit.Framework;
+using Assert = Xunit.Assert;
 
 namespace DatapayCalculatorTests
 {
     public class TaxCalculatorTests
     {
-       
+        private TaxCalculator _taxCalculator;
+
+        public TaxCalculatorTests()
+        {
+            _taxCalculator = new TaxCalculator();
+        }
+        
+        public void testCalculateTax()
+        {
+            Assert.Matches(1400.ToString(), _taxCalculator.CalculateTax(14000, "ASB").ToString());
+        }
+
     }
 }
